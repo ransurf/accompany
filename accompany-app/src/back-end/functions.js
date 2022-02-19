6d2d8551d96e08f929afc2694533ffde39f3b693
Satvik Garg <sat.garg03@gmail.com>
2022-02-18 22:12:04 -0800

    Initialized Firebase and added auth and create

---
+++ accompany-app/src/back-end/functions.js
@@ -0,0 +1,15 @@
+import * as Auth from "./firebase-files/Auth";
+
+// All functions that front-end will call (Keep it simple)
+const signUp = async (email, password, username, name) => {
+  const user = await Auth.signUpProfessor(email, password, username, name);
+  return user;
+};
+
+const signIn = async (email, password) => {
+  const user = await Auth.signIn(email, password);
+  return user;
+};
+
+//get user information //video, video title, video description, associated aassignments
+export { signUp, signIn };
