3fa3e85399a14ceb0634e5e495b53566f9a75eb5
ransurf <ransomguest@gmail.com>
2022-02-19 23:11:29 -0800

    more intuitive header display for login/logout status

--- accompany-app/src/components/PageHeader.js
+++ accompany-app/src/components/PageHeader.js
@@ -1,5 +1,6 @@
-import {React} from 'react';
+import { React } from 'react';
 import MainPage from '../pages/MainPage';
+import LandingPage from '../pages/LandingPage';
 import LoginPage from '../pages/LoginPage';
 import SignUpPage from '../pages/SignUpPage';
 import QuotesPage from '../pages/QuotesPage';
@@ -9,9 +10,11 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { signOut } from "../back-end/functions.js";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import ChatroomApp from "../pages/Chatroom";
+import Cookies from 'js-cookie';
 
 function signOutAndRedirect() {
   signOut();
+  Cookies.remove('user');
   window.location.href = "/";
 }
 
@@ -22,15 +25,15 @@ function PageHeader() {
       <Router>
         <div className="header">
           <div className="links">
-            <Link className="link" to="/home">
-              Home
-            </Link>
-            {/* <Link className="link" to="/login">Login</Link> */}
-            <Link className="link" to="/quotespage">
-              Quotes
-            </Link>
             {user ? (
               <>
+                <Link className="link" to="/home">
+                  Home
+                </Link>
+                {/* <Link className="link" to="/login">Login</Link> */}
+                <Link className="link" to="/quotespage">
+                  Quotes
+                </Link>
                 <Link className="link" to="/chatroom">
                   ChatRoom
                 </Link>
@@ -39,14 +42,20 @@ function PageHeader() {
                 </Link>
               </>
             ) : (
-              <Link className="link" to="/login">
-                Login
-              </Link>
+              <>
+                <Link className="link" to="/">
+                  Accompany
+                </Link>
+                <Link className="link" to="/login">
+                  Login
+                </Link>
+              </>
             )}
           </div>
         </div>
         <div className="content">
           <Routes>
+            <Route exact path="/" element={<LandingPage />} />
             <Route exact path="/home" element={<MainPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignUpPage />} />
---
+++ accompany-app/src/pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div>
+        <h3>This is a captivating slogan</h3>
+        <p>Accompany is a social app that allows users to share wellness advice and have group therapy sessions</p>
+    </div>
+  )
+}
+
+export default LandingPage
