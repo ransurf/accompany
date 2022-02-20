0d6d1f3ff5cad2889654d6d153b48be84b5d4842
myck-prog <myckland.learnprog@gmail.com>
2022-02-19 17:10:42 -0800

    te Card Object

--- accompany-app/src/pages/QuotesPage.js
+++ accompany-app/src/pages/QuotesPage.js
@@ -13,12 +13,35 @@ import { ClassNames } from '@emotion/react';
 
 
 function QuotesPage() {
+    const [title, setTitle] = useState('')
+    const [details, setDetails] = useState('')
+    const [titleError, setTitleError] = useState(false)
+    const [detailsError, setDetailsError] = useState(false)
     
+    var quote = {}
+    // Handling Submit 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setTitleError(false)
+        setDetailsError(false)
+        if(title == ''){
+            setTitleError(true)
+        }
+        if(details == ''){
+            setDetailsError(true)
+        }
+
+        if(title && details){
+            // Set this to as a property for Card
+           
+            console.log(title,details)
+        }
+    }
 
     return (
         <Container>
             <Typography variant="h2" color="primary" align="center" gutterBottom display="block">
-                Your Quotes, namea
+                Your Quotes, name
             </Typography>
             {/* <QuotesLayout/> */}
 
@@ -37,22 +60,27 @@ function QuotesPage() {
            
             <br />
             {/* Create new Quote */}
-            <form noValidate autoComplete="off">
+            <form noValidate autoComplete="off" onSubmit={handleSubmit}>
                 <TextField
-                
+                onChange = {(e)=> setTitle(e.target.value)}
                     label="Quote Title"
                     variant="outlined"
                     fullWidth
-                    required />
+                    required
+                    error = {titleError} />
     
                 <TextField
+                 onChange = {(e)=> setDetails(e.target.value)}
                     label="Quote"
                     variant="outlined"
                     multiline
-                    maxRows={4}
-                    minRows={2}
+                    maxRows={5}
+                    minRows={3}
                     fullWidth
-                    required />
+                    required 
+                    error ={detailsError}
+                    />
+                    <Button type="submit">Submit</Button>
             </form>
 
 
